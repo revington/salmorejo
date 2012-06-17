551a2d9cd894d275b2c4432f2569cd9288364a0e
Pedro Narciso García Revington <p.revington@gmail.com>
2012-06-18 00:48:21 +0200

    Fixed a missed reference

--- salmorejo/Salmorejo.System.Web/HttpContextExtensions.cs
+++ salmorejo/Salmorejo.System.Web/HttpContextExtensions.cs
@@ -21,6 +21,8 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 // 
 using System;
+using System.Web;
+using System.IO.Compression;
 
 namespace Salmorejo.System.Web
 {
